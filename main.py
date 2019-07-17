3d10c146bcd7a0586ff82f1624f858ee5df66b4f
Mikael SAMAK <msamak@student.42.fr>
2019-07-17 15:06:19 +0200

    regex version

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
-from tools import *
-from parsing import *
+import sys
+import argparse
+from pprint import pprint
+import re
+
+def check_equality(input):
+	if input.count("=") != 1:
+		print("Error: An equation must contain (only) one \"=\".")
+		exit()
+
+def split_equality(input):
+	input = re.sub(r"\s", "", input)
+	left, right = input.split("=")
+	if not left.strip() or not right.strip():
+		print("Error: One side of your equation is empty.")
+		exit()
+	else:
+		return (left, right)
+
+def parse(input):
+	terms = {}
+	curr_len = 0
+	left, right = split_equality(input)
+	reg = "(?P<coef>[-+]?\d+\.\d+|[-+]?\d+)\*X\^(?P<exposant>\d+)"
+	for match in re.finditer(reg, left):
+		terms[int(match.group('exposant'))] = float(match.group('coef'))
+		curr_len += len(match.group(0))
+	for match in re.finditer(reg, right):
+		if int(match.group('exposant')) in terms:
+			terms[int(match.group('exposant'))] = terms[int(match.group('exposant'))] -float(match.group('coef'))
+		else:
+			terms[int(match.group('exposant'))] = -float(match.group('coef'))
+		curr_len += len(match.group(0))
+	if curr_len != (len(left) + len(right)):
+		print("Error: Invalid Syntax.")
+		exit()
+	else:
+		return (terms)
+
+def solve(input):
+	check_equality(input)
+	terms = parse(input)
+
+def get_user_input():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("equation", help="Solve Polynomial Equation")
+	args = parser.parse_args()
+	return (args.equation)
 
 def main():
 
-	parsing_obj = Parsing()
-	parsing_obj.get_user_input()
-	parsing_obj.check_format()
-	if parsing_obj.invalid_syntax == False:
-		parsing_obj.split_equality()
-		parsing_obj.store_variable()
-		print_reduced_form(parsing_obj)
-		print_polynomial_degree(parsing_obj)
+	input = get_user_input()
+	solve(input)
 
 if __name__== "__main__":
   main()
